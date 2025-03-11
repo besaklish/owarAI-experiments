dd009c114056295dff25db72d87a69bb918e0d38
besaklish <besaklish@gmail.com>
2025-03-11 22:51:46 +0900

    feat: experimenting build and deploy to GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      src: fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // Use BASE_PATH from environment variables, default to '/' if not set
+  const basePath = env.BASE_PATH || '/'
+
+  return {
+    base: basePath,
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        src: fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })

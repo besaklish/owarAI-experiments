126b1ed093e4b58601ba870b2286a521a94ca2a8
besaklish <besaklish@gmail.com>
2025-03-12 18:53:35 +0900

    feat: update TopView to present features

--- src/features/top/views/TopView.vue
+++ src/features/top/views/TopView.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>
-    <Button>yay</Button>
-    <InputText type="text" v-model="value" />
-  </div>
+  <Card>
+    <template #title> owarAI experiments </template>
+    <template #content>
+      <div>
+        <Card>
+          <template #title> owarAI Script Generator </template>
+          <template #content> OwarAI scripts are generated based on the theme you input. </template>
+        </Card>
+        <Card>
+          <template #title> owarAI Dynamic UI </template>
+          <template #content> Dynamic UI is presented for you. </template>
+        </Card>
+      </div>
+    </template>
+  </Card>
 </template>
 
 <script setup lang="ts">
-import { Button } from 'primevue'
-import InputText from 'primevue/inputtext'
-import { ref } from 'vue'
-
-const value = ref('')
+import { Card } from 'primevue'
 </script>

6f1b3d1f55c82e035d0a69868493b7219383fbbd
besaklish <besaklish@gmail.com>
2025-03-12 18:39:36 +0900

    feat: experiment prime vue

--- src/features/top/views/TopView.vue
+++ src/features/top/views/TopView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>Hellow orkls</div>
+  <div>
+    <Button>yay</Button>
+    <InputText type="text" v-model="value" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from 'primevue'
+import InputText from 'primevue/inputtext'
+import { ref } from 'vue'
+
+const value = ref('')
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { createApp } from 'vue'
 import App from 'src/app/App.vue'
 import { router } from 'src/app/router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 const app = createApp(App)
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      prefix: 'p',
+      darkModeSelector: false,
+    },
+  },
+})
 app.mount('#app')

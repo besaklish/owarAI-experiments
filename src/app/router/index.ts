0914b360f612015f477db77c3b8093a235fa1432
besaklish <besaklish@gmail.com>
2025-03-12 19:16:51 +0900

    build: fix routing issue with GH pages

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,7 +1,7 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import { routes } from 'src/app/router/routes'
 
-const base = import.meta.env.BASE_PATH || '/'
+const base = import.meta.env.BASE_URL
 
 export const router = createRouter({
   history: createWebHistory(base),
